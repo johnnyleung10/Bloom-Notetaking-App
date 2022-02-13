2f22422050d748377ba929e6110bc729216dce02
j82leung <johnny.leung@uwaterloo.ca>
2022-02-13 16:40:17 -0500

    Merged FileModel into Model-Database branch

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/models/NoteModel.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/models/NoteModel.kt
@@ -2,29 +2,16 @@ package com.example.notetakingapp.models
 
 import android.content.Context
 import android.text.SpannableStringBuilder
-import android.text.format.DateFormat.format
 import androidx.core.text.toHtml
 import com.example.notetakingapp.models.sqlite.DatabaseHelper
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
-abstract class NoteModel(
-    var title : String,
-    var contents : SpannableStringBuilder,
-    var currFolder : String,
-    var context: Context
-) {
-    var folderID : Int = -1
-    var noteID : Long = -1
+class NoteModel(
+    title : String,
+    context: Context
+) : FileModel(title, context) {
 
-    val isoFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-    val dateCreated : LocalDateTime = LocalDateTime.now()
-    var lastModifiedDate : LocalDateTime = dateCreated
-    var deletionDate : LocalDateTime? = null
-
-    /**
-     * On note creation, sync with SQLite
-     */
+    var contents : SpannableStringBuilder = SpannableStringBuilder("")
+    var currFolder : String = ""
 
     /**
      * Converts spannableString to HTML for storage
@@ -33,31 +20,19 @@ abstract class NoteModel(
         return contents.toHtml()
     }
 
-    private fun dateToISO(date : LocalDateTime?) : String {
-        if (date == null) return("")
-        return date.format(isoFormat)
-    }
-    fun getDateCreated() : String {return dateToISO(dateCreated)}
-    fun getLastModifiedDate() : String {return dateToISO(lastModifiedDate)}
-    fun getDeletionDate() : String {return dateToISO(deletionDate)}
-
     /**
      * Delete note, sends to recently deleted folder
      */
     fun deleteNote() {
-        lastModifiedDate = LocalDateTime.now()
-        deletionDate = lastModifiedDate
         currFolder = "Recently Deleted"
-        folderID = -1
+        deleteFile()
     }
 
     /**
      * Restore note from recently deleted
      */
     fun restoreNote() {
-        lastModifiedDate = LocalDateTime.now()
-        deletionDate = null
         currFolder = "Recently Deleted"  // Bring back to original folder
-        folderID = -1                    // Bring back to original folder
+        deleteFile()
     }
 }
