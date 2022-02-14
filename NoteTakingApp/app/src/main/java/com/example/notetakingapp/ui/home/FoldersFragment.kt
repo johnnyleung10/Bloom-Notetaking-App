7e3de244d67221f93e216dcf06905ae892820855
Lucas Harvey <l4harvey@uwaterloo.ca>
2022-02-14 12:35:49 -0500

    style dialog for new folder

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/FoldersFragment.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/FoldersFragment.kt
@@ -1,13 +1,10 @@
 package com.example.notetakingapp.ui.home
 
 import FoldersRecyclerViewAdapter
-import android.R
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -73,7 +70,7 @@ class FoldersFragment : Fragment() {
     }
 
     private fun createNewFolder() {
-        val dialogFragment = CreateFolderDialogFragment()
+        val dialogFragment = NewFolderDialogFragment()
         dialogFragment.show(childFragmentManager, "create_folder")
     }
 }
--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/NewFolderDialogFragment.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/NewFolderDialogFragment.kt
@@ -9,23 +9,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.example.notetakingapp.R
 
-class CreateFolderDialogFragment : DialogFragment() {
+class NewFolderDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-            // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
-            builder.setMessage("Enter Folder Name")
-                .setPositiveButton("Create",
+            // Get the layout inflater
+            val inflater = requireActivity().layoutInflater;
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.fragment_create_folder_dialog, null))
+                // Add action buttons
+                .setPositiveButton(R.string.create,
                     DialogInterface.OnClickListener { dialog, id ->
-                        // FIRE ZE MISSILES!
+                        // sign in the user ...
                     })
-                .setNegativeButton("Cancel",
+                .setNegativeButton(R.string.cancel,
                     DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
+                        getDialog()?.cancel()
                     })
-            // Create the AlertDialog object and return it
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
