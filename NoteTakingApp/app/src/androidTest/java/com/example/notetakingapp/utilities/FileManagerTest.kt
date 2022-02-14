822cc96ae84196a5a418447447bab81c411ed4e2
j82leung <johnny.leung@uwaterloo.ca>
2022-02-14 12:22:16 -0500

    Folder tests

--- NoteTakingApp/app/src/androidTest/java/com/example/notetakingapp/utilities/FileManagerTest.kt
+++ NoteTakingApp/app/src/androidTest/java/com/example/notetakingapp/utilities/FileManagerTest.kt
@@ -6,8 +6,6 @@ import org.junit.Assert
 import org.junit.Test
 
 internal class FileManagerTest {
-    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-
     @Test
     fun getFolderList() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
@@ -21,22 +19,38 @@ internal class FileManagerTest {
         val manager = FileManager(appContext)
         manager.initFiles()
         Assert.assertEquals(2, manager.folderList.size)
+        Assert.assertEquals("Uncategorized", manager.folderList[1]?.title!!)
+        Assert.assertEquals("Recently Deleted", manager.folderList[2]?.title!!)
     }
 
     @Test
     fun createNewFolder() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         val manager = FileManager(appContext)
-        //manager.createNewFolder()
-        //Assert.assertEquals(2, manager.folderList.size)
+        manager.initFiles()
+        manager.createNewFolder("New Folder 1")
+        Assert.assertEquals(3, manager.folderList.size)
+        Assert.assertEquals("New Folder 1", manager.folderList[3]?.title!!)
     }
 
     @Test
     fun editFolder() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val manager = FileManager(appContext)
+        manager.initFiles()
+        manager.createNewFolder("New Folder 1")
+        manager.editFolder(3, title = "New Folder 3")
+        Assert.assertEquals("New Folder 3", manager.folderList[3]?.title!!)
     }
 
     @Test
     fun deleteFolder() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val manager = FileManager(appContext)
+        manager.initFiles()
+        manager.createNewFolder("New Folder 1")
+        manager.deleteFolder(3)
+        Assert.assertEquals("New Folder 2", manager.folderList.size)
     }
 
     @Test
