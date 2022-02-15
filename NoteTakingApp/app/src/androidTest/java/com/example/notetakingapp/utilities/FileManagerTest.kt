19f00467e7eb14e0642cc7306a3db3a43403f9af
j82leung <johnny.leung@uwaterloo.ca>
2022-02-15 11:29:17 -0500

    Test cases

--- NoteTakingApp/app/src/androidTest/java/com/example/notetakingapp/utilities/FileManagerTest.kt
+++ NoteTakingApp/app/src/androidTest/java/com/example/notetakingapp/utilities/FileManagerTest.kt
@@ -56,6 +56,21 @@ internal class FileManagerTest {
     */
 
     @Test
+    fun fileManagerTesting() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val manager = FileManager()
+        manager.setContext(appContext)
+        Assert.assertEquals(HashMap<Long, FolderModel>(), manager.folderList)
+        manager.initFiles()
+        Assert.assertEquals(2, manager.folderList.size)
+        Assert.assertEquals("Uncategorized", manager.folderList[1]?.title!!)
+        Assert.assertEquals("Recently Deleted", manager.folderList[2]?.title!!)
+        manager.createNewFolder("New Folder 1")
+        Assert.assertEquals(3, manager.folderList.size)
+        Assert.assertEquals("New Folder 1", manager.folderList[3]?.title!!)
+    }
+
+    @Test
     fun createNewNote() {
     }
 
