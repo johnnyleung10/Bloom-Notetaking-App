5a423619a0805de01dfca5929be10703473a9076
ajaysharma <a388shar@uwaterloo.ca>
2022-02-16 21:00:33 -0500

    created fragments and nav for edit note

---
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/EditNoteFragment.kt
@@ -0,0 +1,32 @@
+package com.example.notetakingapp.ui.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notetakingapp.R
+
+class EditNoteFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EditNoteFragment()
+    }
+
+    private lateinit var viewModel: EditNoteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditNoteViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/home/EditNoteViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.notetakingapp.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class EditNoteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
