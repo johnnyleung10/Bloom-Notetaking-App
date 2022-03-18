1f5dfef0ee8afb3db1fd47fda6c4283005456a42
ajaysharma <a388shar@uwaterloo.ca>
2022-03-18 11:05:30 -0400

    Added DB helper functions

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/models/sqlite/DailyEntryDatabaseHelper.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/models/sqlite/DailyEntryDatabaseHelper.kt
@@ -8,6 +8,7 @@ import android.provider.BaseColumns
 import com.example.notetakingapp.models.DailyEntryModel
 import com.example.notetakingapp.models.DailyPromptModel
 import com.example.notetakingapp.models.MoodModel
+import com.example.notetakingapp.models.NoteModel
 
 private const val SQL_CREATE_DAILY_ENTRIES =
     "CREATE TABLE ${DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.TABLE_NAME} (" +
@@ -21,14 +22,9 @@ private const val SQL_CREATE_DAILY_ENTRIES =
             "${DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_DATE_MODIFIED} TEXT," +
             "${DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_DATE_DELETED} TEXT," +
             "FOREIGN KEY ("+DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_DAILY_PROMPT_ID+") REFERENCES "+DailyEntryDatabaseHelper.DatabaseContract.DailyPrompt.TABLE_NAME+"("+BaseColumns._ID+")," +
-            "FOREIGN KEY ("+DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_LINKED_NOTE_ID+") REFERENCES "+NoteTakingDatabaseHelper.DatabaseContract.NoteEntry.TABLE_NAME+"("+BaseColumns._ID+")," +
-            "FOREIGN KEY ("+DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_MOOD_ID+") REFERENCES "+DailyEntryDatabaseHelper.DatabaseContract.Mood.TABLE_NAME+"("+BaseColumns._ID+"))"
+            "FOREIGN KEY ("+DailyEntryDatabaseHelper.DatabaseContract.DailyEntry.COLUMN_NAME_LINKED_NOTE_ID+") REFERENCES "+NoteTakingDatabaseHelper.DatabaseContract.NoteEntry.TABLE_NAME+"("+BaseColumns._ID+"))"
 
-private const val SQL_CREATE_MOODS =
-    "CREATE TABLE ${DailyEntryDatabaseHelper.DatabaseContract.Mood.TABLE_NAME} (" +
-            "${DailyEntryDatabaseHelper.DatabaseContract.Mood.COLUMN_NAME_ID} INTEGER PRIMARY KEY," +
-            "${DailyEntryDatabaseHelper.DatabaseContract.Mood.COLUMN_NAME_DESCRIPTION} TEXT," +
-            "${DailyEntryDatabaseHelper.DatabaseContract.Mood.COLUMN_NAME_COLOUR} TEXT)"
+
 
 private const val SQL_CREATE_DAILY_PROMPTS =
     "CREATE TABLE ${DailyEntryDatabaseHelper.DatabaseContract.DailyPrompt.TABLE_NAME} (" +
@@ -60,18 +56,6 @@ class DailyEntryDatabaseHelper(private val context: Context) :
         return id
     }
 
-    fun insertMood(mood: MoodModel): Long {
-        val values = ContentValues().apply {
-            put(Mood.COLUMN_NAME_DESCRIPTION, mood.description)
-            put(Mood.COLUMN_NAME_COLOUR, mood.colour)
-        }
-        val dbWrite = this.writableDatabase
-        val id = dbWrite.insert(Mood.TABLE_NAME, null, values)
-        mood.id = id
-        dbWrite.close()
-        return id
-    }
-
     fun insertDailyPrompt(dailyPrompt: DailyPromptModel): Long {
         val values = ContentValues().apply {
             put(DailyPrompt.COLUMN_NAME_PROMPT, dailyPrompt.prompt)
@@ -84,10 +68,64 @@ class DailyEntryDatabaseHelper(private val context: Context) :
     }
 
     // QUERYING
+    fun getAllDailyEntries() : List<DailyEntryModel>{
+        val retList : ArrayList<DailyEntryModel> = arrayListOf()
+        val queryString = "SELECT * FROM " + DatabaseContract.DailyEntry.TABLE_NAME
+
+        val dbRead = this.readableDatabase
+
+        val cursor = dbRead.rawQuery(queryString, null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val id = cursor.getInt(0)
+                val dailyPromptId = cursor.getInt(1)
+                val dailyPromptAnswer = cursor.getString(2)
+                val dailyImage = cursor.getBlob(3)
+                val linkedNoteId = cursor.getInt(4)
+                val moodId = cursor.getInt(5)
+                val dateCreated = cursor.getString(6)
+                val dateModified = cursor.getString(7)
+                val dateDeleted = cursor.getString(8)
+
+                val dailyEntry = DailyEntryModel("JOHNNY GET RID OF TITLE",
+                    context, id.toLong(), linkedNoteId.toLong(), dailyPromptId.toLong(),
+                    dailyPromptAnswer, moodId.toLong(), dailyImage, dateCreated, dateModified,
+                    dateDeleted)
+                retList.add(dailyEntry)
+            } while (cursor.moveToNext())
+        }
+
+        dbRead.close()
+        cursor.close()
+        return retList
+    }
+
+    fun getAllPrompts() : List<DailyPromptModel>{
+        val retList : ArrayList<DailyPromptModel> = arrayListOf()
+        val queryString = "SELECT * FROM " + DatabaseContract.DailyPrompt.TABLE_NAME
+
+        val dbRead = this.readableDatabase
+
+        val cursor = dbRead.rawQuery(queryString, null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val id = cursor.getLong(0)
+                val promptText = cursor.getString(1)
+
+                val dailyPrompt = DailyPromptModel(id, promptText)
+                retList.add(dailyPrompt)
+            } while (cursor.moveToNext())
+        }
+
+        dbRead.close()
+        cursor.close()
+        return retList
+    }
 
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(SQL_CREATE_DAILY_ENTRIES)
-        db?.execSQL(SQL_CREATE_MOODS)
         db?.execSQL(SQL_CREATE_DAILY_PROMPTS)
     }
 
@@ -112,12 +150,6 @@ class DailyEntryDatabaseHelper(private val context: Context) :
             const val COLUMN_NAME_LINKED_NOTE_ID = "linked_note_id"
         }
 
-        object Mood : BaseColumns {
-            const val TABLE_NAME = "mood_table"
-            const val COLUMN_NAME_ID = BaseColumns._ID
-            const val COLUMN_NAME_DESCRIPTION = "description"
-            const val COLUMN_NAME_COLOUR = "colour"
-        }
 
         object DailyPrompt : BaseColumns {
             const val TABLE_NAME = "daily_prompt_table"
