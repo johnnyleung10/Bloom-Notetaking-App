a4267b250f7c1b5e220e3d8dd363cd85c509e766
yolanda <y369yu@uwaterloo.ca>
2022-03-18 04:34:08 -0400

    Added colours to calendar and prompt view

---
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/calendar/CalendarAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.notetakingapp.ui.calendar
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
+import com.example.notetakingapp.R
+
+
+class CalendarAdapter(private val mContext: Context, private val colors: ArrayList<String>) : ArrayAdapter<String>(mContext, 0, colors) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var color_cell = convertView
+        if (color_cell == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            color_cell = LayoutInflater.from(parent.context).inflate(R.layout.calendar_cell, parent, false)
+        }
+
+        val hex: String? = getItem(position)
+        val block: View = color_cell!!.findViewById(R.id.block)
+
+        val colorId: Int = context.resources.getIdentifier(hex, "color", context.packageName)
+        val color = ContextCompat.getColor(context, colorId)
+
+        block.setBackgroundColor(color)
+
+        return color_cell
+    }
+
+    fun setColors(newColors: ArrayList<String>){
+        colors.clear()
+        colors.addAll(newColors)
+        this.notifyDataSetChanged()
+    }
+}
--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/calendar/CalendarFragment.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/calendar/CalendarFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.notetakingapp.databinding.FragmentCalendarBinding
+import io.ktor.util.date.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CalendarFragment : Fragment() {
 
     private lateinit var calendarViewModel: CalendarViewModel
     private var _binding: FragmentCalendarBinding? = null
+    private lateinit var adapter: CalendarAdapter
+//    private var selectedMonth = 0
+//    private var selectedDate = 0
+//    private var selectedYear = 0
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,17 +32,50 @@ class CalendarFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         calendarViewModel =
             ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textCalendar
-//        calendarViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+        val grid = binding.colorBlocks
+        val colors = ArrayList<String>()
+
+        val c: Calendar = Calendar.getInstance()
+//        selectedDate = Calendar.DATE
+//        selectedMonth = Calendar.MONTH - 1
+//        selectedYear = Calendar.YEAR
+
+        c.set(Calendar.DAY_OF_MONTH, 0)
+        val padding: Int = c.get(Calendar.DAY_OF_WEEK) //- (c.get(Calendar.DATE) % 7)
+
+        for (i in 1..padding) colors.add("none")
+        colors.add("happy")
+        colors.add("sad")
+        colors.add("none")
+        colors.add("neutral")
+        colors.add("none")
+        colors.add("loving")
+        colors.add("neutral")
+        colors.add("angry")
+        colors.add("none")
+        colors.add("excited")
+        colors.add("doubtful")
+        colors.add("none")
+        colors.add("neutral")
+        colors.add("loving")
+        colors.add("none")
+        colors.add("happy")
+        colors.add("sad")
+        colors.add("none")
+        colors.add("neutral")
+        colors.add("none")
+        colors.add("neutral")
+        colors.add("excited")
+
+        adapter = CalendarAdapter(requireContext(), colors)
+        grid.adapter = adapter
 
         // Setup all listeners for fragment
         addListeners()
@@ -49,16 +91,48 @@ class CalendarFragment : Fragment() {
     private fun addListeners(){
         val promptButton: Button = binding.prompt
         val calendar: CalendarView = binding.calendar
+        val prompt: CardView = binding.entry
         val delete: ImageButton = binding.delete
+        val left: Button = binding.left
+        val right: Button = binding.right
+
+        left.setOnClickListener {
+            val cal = Calendar.getInstance()
+            cal.time = Date(calendar.date)
+            cal.add(Calendar.MONTH, -1)
+            cal.set(Calendar.DAY_OF_MONTH, 1)
+            calendar.date = cal.time.time
+            right.isVisible = true
+        }
+
+        right.setOnClickListener {
+            val cal = Calendar.getInstance()
+            cal.time = Date(calendar.date)
+            cal.add(Calendar.MONTH, 1)
+            cal.set(Calendar.DAY_OF_MONTH, 1)
+            calendar.date = cal.time.time
+
+            if (Calendar.MONTH == cal.get(Calendar.MONTH))
+                right.isVisible = false
+        }
 
         promptButton.setOnClickListener{
             onPromptClick()
         }
 
+        calendar.maxDate = getTimeMillis()
         calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
-
+//            prompt.isVisible = false
+//            if (year == selectedYear && month == selectedMonth && dayOfMonth == selectedDate){
+//                val newColors = ArrayList<String>()
+//                for (i in 1..30) newColors.add("none")
+//                adapter.setColors(newColors)
+//            } else {
+//                selectedYear = year
+//                selectedMonth = month
+//                selectedDate = dayOfMonth
+//            }
         }
-
     }
 
     private fun onPromptClick() {
