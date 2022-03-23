081835493f830f3e169f1c8fc5d1b9e1e3023a52
Lucas Harvey <l4harvey@uwaterloo.ca>
2022-03-23 12:27:46 -0400

    fix padding offset

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/calendar/CalendarFragment.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/ui/calendar/CalendarFragment.kt
@@ -6,11 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.map
 import androidx.navigation.fragment.NavHostFragment
 import com.example.notetakingapp.R
 import com.example.notetakingapp.databinding.FragmentCalendarBinding
@@ -18,9 +16,7 @@ import com.example.notetakingapp.models.DailyEntryModel
 import com.example.notetakingapp.utilities.DailyEntryManager
 import io.ktor.util.date.*
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.collections.ArrayList
 
 class CalendarFragment : Fragment() {
 
@@ -53,11 +49,8 @@ class CalendarFragment : Fragment() {
         val grid = binding.colorBlocks
 
         val c: Calendar = Calendar.getInstance()
-        val today = LocalDateTime.now()
 
-        c.set(Calendar.DAY_OF_MONTH, today.dayOfMonth)
-        c.set(Calendar.MONTH, today.monthValue)
-        c.set(Calendar.YEAR, today.year)
+        setCalendarToToday()
 
         val colors = ArrayList<Int>()
         adapter = CalendarAdapter(requireContext(), colors)
@@ -77,18 +70,29 @@ class CalendarFragment : Fragment() {
         _binding = null
     }
 
+    private fun setCalendarToToday(){
+        val c: Calendar = Calendar.getInstance()
+        val today = LocalDateTime.now()
+        c.set(Calendar.DAY_OF_MONTH, today.dayOfMonth)
+        c.set(Calendar.MONTH, today.monthValue)
+        c.set(Calendar.YEAR, today.year)
+    }
+
     private fun getColorsForDailyEntries(entries: HashMap<Int, DailyEntryModel>): ArrayList<Int>{
         val c: Calendar = Calendar.getInstance()
 
-        val padding: Int = c.get(Calendar.DAY_OF_WEEK) //- (c.get(Calendar.DATE) % 7)
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.DAY_OF_MONTH, 0)
+        val padding: Int = c.get(Calendar.DAY_OF_WEEK) - 1
+        setCalendarToToday()
+
         val newColors = ArrayList<Int>()
         for (i in 1..padding) newColors.add(R.color.none)
 
         val numDaysInMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH)
-        val dailyEntries = calendarViewModel.dailyEntries.value!!
         for(currDay in 1..numDaysInMonth){
-            if(currDay in dailyEntries){
-                newColors.add(dailyEntries[currDay]!!.mood.colour)
+            if(currDay in entries){
+                newColors.add(entries[currDay]!!.mood.colour)
             } else {
                 newColors.add(R.color.none)
             }
@@ -145,8 +149,8 @@ class CalendarFragment : Fragment() {
         }
     }
 
-    private fun getDailyEntriesForMonth(month: Int, year: Int){
-        val dailyEntries = dailyEntryManager.getDailyEntriesByDate(month, year)
+    private fun getDailyEntriesForMonth(monthIndex: Int, year: Int){
+        val dailyEntries = dailyEntryManager.getDailyEntriesByDate(monthIndex + 1, year)
         calendarViewModel.setDailyEntries(dailyEntries)
     }
 
